b5a649cc85e675f0734dde40cfb69b001677db73
cearnach <cearnach@foxmail.com>
2018-08-07 17:05:40 +0800

    v1.0

--- mall-seller/seller-feign/src/main/java/com/xmut/osm/seller/feign/SellerServiceClient.java
+++ mall-seller/seller-feign/src/main/java/com/xmut/osm/seller/feign/SellerServiceClient.java
@@ -1,9 +1,13 @@
 package com.xmut.osm.seller.feign;
 
+import com.xmut.osm.common.bean.PageInfo;
 import com.xmut.osm.common.constraint.FeignClientConstraints;
 import com.xmut.osm.common.util.BaseServiceClient;
 import com.xmut.osm.entity.Seller;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author 阮胜
@@ -11,4 +15,16 @@ import org.springframework.cloud.openfeign.FeignClient;
  */
 @FeignClient(name = FeignClientConstraints.SELELR_SERVICE_CLIENT_NAME)
 public interface SellerServiceClient extends BaseServiceClient<Seller> {
+    @GetMapping("/seller/all?page={page}&size={size}")
+
+    @Override
+    PageInfo<Seller> fetchAll(@PathVariable Integer page, @PathVariable Integer size);
+
+    @PostMapping("/seller/save")
+    @Override
+    boolean save(@RequestBody Seller seller);
+
+    @DeleteMapping("/seller/deleteAll")
+    @Override
+    List<Integer> deleteAll(@RequestParam("ids") Integer[] ids);
 }
